1c180a0aa668bd0bf548fa8504cc0aa2ab7710b4
ROhit-Kumar-Guin <rohitguin006@gmail.com>
2025-08-08 22:58:18 +0530

    updated proxy for cors frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,11 +4,11 @@ module.exports = function (root) {
     root.use(
     '/api', // Adjust the path you want to proxy
     createProxyMiddleware({
-      target: 'http://localhost:8000', // Specify the address of your backend server
+      target: 'https://tech-blog-backend-ten.vercel.app', // Specify the address of your backend server
       changeOrigin: true,
       secure: false, // Set to false if your backend doesn't use HTTPS
       headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:3000', // Adjust the React app's origin
+        'Access-Control-Allow-Origin': 'https://techbloggy.vercel.app', // Adjust the React app's origin
       },
     })
   );
